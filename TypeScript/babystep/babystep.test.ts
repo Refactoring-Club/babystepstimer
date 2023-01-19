3095bd058841ab2b1e447035db87af8111ec888c
Ray Veliz <rveliz@acm.org>
2023-01-18 21:09:19 -0500

    Add test for createHtml when timer is running

--- TypeScript/babystep/babystep.test.ts
+++ TypeScript/babystep/babystep.test.ts
@@ -1,9 +1,16 @@
 const { CreateTimerHtml } = require("./babystep");
 
 describe("createHtml", () => {
-  test("returns html", () => {
-    const html =
-      '<div style="border: 3px solid #555555; background: magenta; margin: 0; padding: 0;"><h1 style="text-align: center; font-size: 30px; color: magenta;">I\'m a timer</h1><div style="text-align: center"><a style="color: #555555;" href="javascript:command(\'start\');">Start</a> <a style="color: #555555;" href="javascript:command(\'quit\');">Quit</a> </div></div>';
-    expect(CreateTimerHtml("I'm a timer", "magenta", false)).toBe(html);
+  test("html for not running timer", () => {
+    const htmlPart =
+      /<a style="color: #\d+;" href="javascript:command\('start'\);">Start<\/a>/;
+    expect(CreateTimerHtml("I'm a timer", "magenta", false)).toMatch(htmlPart);
+  });
+
+  test("html for running timer", () => {
+    const htmlPart =
+      '<a style="color: #555555;" href="javascript:command(\'stop\');">Stop</a> ' +
+      '<a style="color: #555555;" href="javascript:command(\'reset\');">Reset</a> ';
+    expect(CreateTimerHtml("I'm a timer", "magenta", true)).toContain(htmlPart);
   });
 });
